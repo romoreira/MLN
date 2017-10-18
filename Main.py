b2457a093e23eac7718aa5078a935c982f890392
rodrigo.moreira206@gmail.com <S3nhagit>
2017-10-18 20:40:16 -0200

    My fist commit

--- Main.py
+++ Main.py
@@ -10,5 +10,12 @@ csv_x = csv_x[1:]
 csv_x.columns = new_header
 
 for columns in csv_x.columns:
-    print("Coluna '"+columns+"':\n%s " % csv_x[columns].astype(float).describe())
+    #print("Coluna '"+columns+"':\n%s " % csv_x[columns].astype(float).describe())
+    print("Coluna '" + columns + "':\n")
+    print("Mean: %0.f" % csv_x[columns].astype(float).mean())
+    print("Maximum: %0.f" % csv_x[columns].astype(float).max())
+    print("Minimum: %0.f" % csv_x[columns].astype(float).min())
+    print("25 Percentil %0.2f" % csv_x[columns].astype(float).quantile(.25))
+    print("90 Percentil %0.2f" % csv_x[columns].astype(float).quantile(.90))
+    print("Standard Deviation: %0.2f" % csv_x[columns].astype(float).std())
     print("\n")
