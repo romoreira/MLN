a510a79bc97547bec3c2cf3114b069374f68223b
rodrigo.moreira206@gmail.com <S3nhagit>
2017-10-21 15:05:47 -0200

    Task II almost complete

--- Main.py
+++ Main.py
@@ -1,16 +1,27 @@
 # 'Author: Rodrigo Moreira'
 
 
+import time
 import pandas as pd
-import matplotlib as plt
+from sklearn.linear_model import LinearRegression
+import numpy as np
+from sklearn.metrics import accuracy_score
+import matplotlib.pyplot as plt
+from sklearn import linear_model
+from sklearn.metrics import mean_absolute_error
 
 csv_x = pd.read_csv('X.csv', sep=',', header=None)
 csv_y = pd.read_csv('Y.csv', sep=',', header=None)
 
-# Esse trecho de codigo retira a primeira linha do DataFrame (que contem os nomes das colunas), cria uma novo DataFrame sem essa primeira linha, depois adiciona as colunas na forma de indices
+# Esse trecho de codigo retira a primeira linha do DataFrame (que contem os nomes das colunas), cria uma novo DataFrame sem essa primeira linha,
+#depois adiciona as colunas na forma de indices
 new_header = csv_x.iloc[0]
 csv_x = csv_x[1:]
 csv_x.columns = new_header
+
+new_header = csv_y.iloc[0]
+csv_y = csv_y[1:]
+csv_y.columns = new_header
 
 for columns in csv_x.columns:
     # print("Coluna '"+columns+"':\n%s " % csv_x[columns].astype(float).describe())
@@ -23,11 +34,7 @@ for columns in csv_x.columns:
     print("Standard Deviation: %0.2f" % csv_x[columns].astype(float).std())
     print("\n")
 
-# -----------------#---------------#----------------#------------------#-----------------#--------------------#-----------------#--------------#
-
-new_header = csv_y.iloc[0]
-csv_y = csv_y[1:]
-csv_y.columns = new_header
+#-----------------#---------------#----------------#------------------#-----------------#--------------------#-----------------#--------------#
 
 for columns in csv_y.columns:
     print("Column '" + columns + "':\n")
@@ -41,8 +48,8 @@ for columns in csv_y.columns:
 
 # (a) the number of observations with memory usage larger than 80%;
 
-print("Number of Observations with memory usage (X..memused) larger than 80%%: %.d" %
-      csv_x[csv_x['X..memused'].astype(float) > 80]['X..memused'].count())
+print("Number of Observations with memory usage (X_memused) larger than 80%%: %.d" %
+      csv_x[csv_x['X_memused'].astype(float) > 80]['X_memused'].count())
 
 # (b) the average number of used TCP sockets for observations with more than 18.000 interrupts/sec;
 
@@ -51,4 +58,59 @@ print("The average number of used TCP socket for observations with more than 18.
 
 # (c) the minimum memory utilization for observations with CPU idle time lower than 20%.
 
-print("The minimum memory utilization for observations with CPU idle time lower than 20%%: %0.2f (X..memused)" %csv_x[csv_x['all_..idle'].astype(float) < 20]['X..memused'].astype(float).min())
+print("The minimum memory utilization for observations with CPU idle time lower than 20%%: %0.2f (X_memused)" %csv_x[csv_x['all_idle'].astype(float) < 20]['X_memused'].astype(float).min())
+
+#-----------------#---------------#----------------#------------------#-----------------#--------------------#-----------------#--------------#
+
+#TASK II
+
+#Calculando a correlacao de ("X_memused") entre as colunas para escolher qual atributo mais impacta na
+#qualidade de servico de entrega de video.
+
+
+for columns in csv_x.columns:
+    csv_x[columns] = csv_x[columns].convert_objects(convert_numeric=True)#Transformando os dados do DataFrameX para tipo Numerico
+
+for columns in csv_y.columns:
+    csv_y[columns] = csv_y[columns].convert_objects(convert_numeric=True)#Transformando os dados do DataFrameY para tipo Numerico
+
+#Check if I am really ignoring the first column (TimeStamp) on calc of the correlation;
+#print("\nCorrelation between 'X_memused' and another columns are:  \n", csv_x.corr()['X_memused']['all_idle':])
+
+#x_train = csv_x.iloc[:-20, csv_x.columns != "TimeStamp"]
+#x_test = csv_x.iloc[-20:, csv_x.columns != "TimeStamp"]
+y_train = csv_y.DispFrames[:-20]
+y_test = csv_y.DispFrames[-20:]
+
+
+x_train = csv_x['X_memused'][:-20]
+x_test = csv_x['X_memused'][-20:]
+
+#Seto novamente a configuracao de DataFrame para nao perder a dimensao
+x_train = pd.DataFrame(x_train)
+x_test = pd.DataFrame(x_test)
+y_train = pd.DataFrame(y_train)
+y_test = pd.DataFrame(y_test)
+
+regr = linear_model.LinearRegression()
+regr.fit(x_train, y_train)
+y_pred = regr.predict(x_test)
+
+# The coefficients
+print("Coefficients: ")
+print(regr.coef_)
+
+# The Normalized mean Absolute Error
+print("The Normalized Mean Absolute Error: %0.2f " % mean_absolute_error(y_test, y_pred))
+
+#print(y_pred)
+#print(y_test)
+
+# Plot outputs
+plt.scatter(x_test, y_test,  color='black')
+plt.plot(x_test, y_pred, color='blue', linewidth=3)
+
+plt.xticks(())
+plt.yticks(())
+
+plt.show()
